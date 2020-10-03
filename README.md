16549399a10bcc56e2f8e8d8419f7ec0e30b1dfe
vishalthengane <vishal@smartail.ai>
2020-10-03 08:43:56 +0530

    added file upload from local.

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - check the link to build the file upload in app.
 
 [link to blog](https://medium.com/dev-genius/a-simple-way-to-build-flask-file-upload-1ccb9462bc2c)
+
--- app/views.py
+++ app/views.py
@@ -5,15 +5,32 @@ import numpy as np
 
 from app import app
 from app.model.infer import predict
-from flask import request, jsonify, render_template
+from flask import request, jsonify, render_template, redirect, flash
+from werkzeug.utils import secure_filename
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
-# app = Flask(__name__)
+app.secret_key = "secret key"
+app.config["max_content_length"] = 16 * 1920 * 1920
+
+path = os.getcwd()
+upload_folder = os.path.join("uploads")
+
+if not os.path.isdir(upload_folder):
+    os.makedirs(upload_folder)
+
+app.config["upload_folder"] = upload_folder
+allowed_extentions = ["png", "jpeg", "jpg", "tiff", "gif"]
+
+
+def allowed_file(file_name):
+    return "." in file_name and file_name.rsplit(".", 
+                        1)[1].lower() in allowed_extentions
+
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("upload.html")
 
 
 @app.route("/test", methods=["POST"])
@@ -33,6 +50,33 @@ def process():
         print(f"{idx} images are done out of {len(image_URLs)}")
         
     return jsonify(out_dict)
+
+
+
+@app.route("/", methods=["POST"])
+def upload_file():
+
+    if request.method == "POST":
+        if "file" not in request.files:
+            flash("No file part.")
+            return redirect(request.url)
+
+    file_ = request.files["file"]
+    if file_.filename == "":
+        flash("No file selected for uploading.")
+        return redirect(request.url)
+
+    if file_ and allowed_file(file_.filename):
+        file_name = secure_filename(file_.filename)
+        file_.save(os.path.join(app.config["upload_folder"], file_name))
+        flash("File uploaded successfully.")
+        return redirect("/")
+
+    else:
+        flash("Allowed file type is ['png', 'jpeg', 'jpg', 'tiff', 'gif']")
+        return redirect(request.url)
+
+    
 
 if __name__ == "app.views":
 
